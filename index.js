304f8736fb44d54b213096a96a4c4047bd496b3d
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2026-07-01 23:29:30 +0900

    animation

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ act.forEach(link => {
         link.classList.add("active");
     }
 })
+
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+        loader.style.opacity = "0";
+
+        setTimeout(() => {
+            loader.remove();
+        }, 600);
+
+    }, 3500);
+
+});
